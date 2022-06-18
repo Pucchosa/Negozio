6c24467adf9a4e3e34958f8b1087f4c2d6d61788
Pucchosa <cdea89@gmail.com>
2022-06-18 17:47:53 +0200

    .

---
+++ Anagrafica/src/GUI/Finestra.java
@@ -0,0 +1,23 @@
+package GUI;
+
+import java.awt.BorderLayout;
+import java.awt.Frame;
+import java.awt.event.ActionEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.beans.PropertyChangeListener;
+
+import javax.swing.Action;
+
+import Negozio.DataBase;
+import Negozio.DataM;
+
+public class Finestra extends Frame{
+	public Finestra (String x){
+		super(x);
+		setLayout(new BorderLayout(100,10));
+		setLocation(150,150);
+		setBackground(Est.chiaro);
+				
+	}
+}
